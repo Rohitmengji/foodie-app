bc82051a61f4bc429acb8d6c794d0161c572dcd5
Rohit Mengji <rohitmengjih@gmail.com>
2023-03-01 13:38:58 +0530

    updated and added authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./components/screens/Login";
+import Home from "./components/screens/Home";
+import SignUp from "../src/components/screens/SignUp";
+import LandingPage from "./components/screens/LandingPage";
+import MyOrder from "./components/screens/MyOrder";
+  import { CartProvider } from "./components/ContextReducer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CartProvider>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<SignUp />} />
+            <Route exact path="/myorder" element={<MyOrder />} />
+            <Route exact path="/landingpage" element={<LandingPage />} />
+          </Routes>
+        </div>
+    </CartProvider>
+    </>
   );
 }
 
